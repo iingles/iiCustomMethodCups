89257d5b3f19f08221fb6c9626b16e333dac49a1
Isaac Ingles <isaac.ingles@gmail.com>
2020-10-27 18:24:04 -0600

    finished project, added images and readme

--- iiCustomMethodCups/MainPage.xaml.cs
+++ iiCustomMethodCups/MainPage.xaml.cs
@@ -10,14 +10,69 @@ namespace iiCustomMethodCups
 {
     public partial class MainPage : ContentPage
     {
+        // Make the conversion multiplier a constant
+        const int CUPS_TO_OUNCES = 8;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        ///   Validate input, calculate cups to ounces, display results
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnConvert_Clicked(object sender, EventArgs e)
         {
+            // Store cups and ounces in a double
+            double cups = 0, 
+                   ounces = 0;
 
+            // Check if cups is valid
+            if (ValidCups(ref cups))
+            {
+                ounces = CupsToOunces(cups);
+                DisplayResults(ounces);
+            }
+        }
+
+
+        /// <summary>
+        ///  Display the results of the calculation
+        /// </summary>
+        /// <param name="ounces"></param>
+        private void DisplayResults(double ounces)
+        {
+            // Change the label text to display the results
+            LblResults.Text = ounces.ToString("n1");
+        }
+
+        /// <summary>
+        ///  convert cups to ounces
+        /// </summary>
+        /// <param name="cups"></param>
+        /// <returns>value for ounces</returns>
+        private double CupsToOunces(double cups)
+        {
+            double ounces = 0;
+            ounces = cups * CUPS_TO_OUNCES;
+            return ounces;
+        }
+
+        /// <summary>
+        ///     Validate cups
+        /// </summary>
+        /// <param name="cups"></param>
+        /// <returns></returns>
+        private bool ValidCups(ref double cups)
+        {
+            // Make sure cups is a number, output and return true cups if valid, return false if not
+            if (double.TryParse(EntryCups.Text, out cups)) return true;
+
+            DisplayAlert("Invalid Input", "Enter a number for cups", "Close");
+
+            return false;
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,18 @@
+# INFO 1200 Participation 11
+### Fall 2020
+
+## Car Design
+
+--- 
+**Code**
+ - [XAML Code](https://github.com/iingles/iiCustomMethodCups/blob/master/iiCustomMethodCups/MainPage.xaml)
+ - [C# Code](https://github.com/iingles/iiCustomMethodCups/blob/master/iiCustomMethodCups/MainPage.xaml.cs)
+
+**Screenshots**
+ - [Screenshot 1](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164151.jpg)
+ - [Screenshot 2](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164203.jpg)
+ - [Screenshot 3](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164217.jpg)
+ - [Screenshot 4](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164226.jpg)
+ - [Screenshot 5](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164241.jpg)
+ - [Screenshot 5](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164255.jpg)
+ - [Screenshot 5](https://github.com/iingles/iiCarDesign/blob/master/Screenshot_20201020-164300.jpg)
